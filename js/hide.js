1b6ffa47ffdf62b7acf08be278fc7a58718f613c
Gordon Bateson <gorodnbateson@gmail.com>
2013-06-30 22:32:17 +0900

    Moodle.2x mod/reader repair hide.js

--- js/hide.js
+++ js/hide.js
@@ -1,3 +1,5 @@
+<script type="text/javascript">
+//<![CDATA[
 var vh_content = new Array();
 function mygetspan(spanid) {
   if (document.getElementById) {
@@ -7,7 +9,7 @@ function mygetspan(spanid) {
   }
   return null;
 }
-function mytoggle(spanid) {
+function toggle(spanid) {
   if (mygetspan(spanid).innerHTML == "") {
     mygetspan(spanid).innerHTML = vh_content[spanid];
     mygetspan(spanid + "indicator").innerHTML = '<img src="' + mywwwroot() + '/mod/reader/pix/open.gif" alt="Opened folder" />';
@@ -28,12 +30,12 @@ function myexpand(spanid) {
   mygetspan(spanid).innerHTML = vh_content[spanid];
   mygetspan(spanid + "indicator").innerHTML = '<img src="' + mywwwroot() + '/mod/reader/pix/open.gif" alt="Opened folder" />';
 }
-function myexpandall() {
+function expandall() {
   for (i = 1; i <= vh_numspans; i++) {
     myexpand("comments_" + String(i));
   }
 }
-function mycollapseall() {
+function collapseall() {
   for (i = vh_numspans; i > 0; i--) {
     mycollapse("comments_" + String(i));
   }
@@ -41,3 +43,5 @@ function mycollapseall() {
 function mywwwroot() {
     return location.href.replace(new RegExp('^(.*?)/mod/reader/.*$'), '$1');
 }
+//]]>
+</script>
