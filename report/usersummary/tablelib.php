b72b2c57896e175eb6b53a9ce5f29ef29695204b
Gordon Bateson <gorodnbateson@gmail.com>
2013-08-16 13:53:54 +0900

    Moodle.2x mod/reader add individual tablelib.php for each report

---
+++ report/usersummary/tablelib.php
@@ -0,0 +1,47 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Create a table to display attempts at a Reader activity
+ *
+ * @package   mod-reader
+ * @copyright 2013 Gordon Bateson <gordon.bateson@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// get parent class
+require_once($CFG->dirroot.'/mod/reader/report/tablelib.php');
+
+/**
+ * usersummary_reader_report_table
+ *
+ * @copyright 2013 Gordon Bateson
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @since     Moodle 2.0
+ */
+class usersummary_reader_report_table extends reader_report_table {
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // functions to format header cells                                           //
+    ////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // functions to format data cells                                             //
+    ////////////////////////////////////////////////////////////////////////////////
+}
