25ef5d18ce86110b0f467741ac324b1ae8a32712
Gordon Bateson <gorodnbateson@gmail.com>
2013-12-27 14:02:10 +0900

    mod/reader remove old download files

--- version.php
+++ version.php
@@ -32,8 +32,8 @@ $module->cron      = 3600;
 $module->component = 'mod_reader';
 $module->maturity  = MATURITY_BETA; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $module->requires  = 2010112400; // Moodle 2.0
-$module->release   = '2013-12-27 (11)';
-$module->version   = 2013122711;
+$module->release   = '2013-12-27 (12)';
+$module->version   = 2013122712;
 
 if (defined('ANY_VERSION')) {
     // Moodle >= 2.2
