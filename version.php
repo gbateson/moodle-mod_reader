59615a001b1f720599e83f79025e4a064961f848
Gordon Bateson <gorodnbateson@gmail.com>
2014-01-13 15:41:58 +0900

    mod/reader fix Unknown column 'version' in 'where clause' when upgrading in Moodle 2.6

--- version.php
+++ version.php
@@ -32,8 +32,8 @@ $module->cron      = 3600;
 $module->component = 'mod_reader';
 $module->maturity  = MATURITY_BETA; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $module->requires  = 2010112400; // Moodle 2.0
-$module->release   = '2014-01-11 (19)';
-$module->version   = 2014011119;
+$module->release   = '2014-01-13 (20)';
+$module->version   = 2014011320;
 
 if (defined('ANY_VERSION')) {
     // Moodle >= 2.2
