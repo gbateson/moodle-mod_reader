9df55908ed3b5960e475ac0800e9e2a0a50054b9
Gordon Bateson <gorodnbateson@gmail.com>
2015-02-01 16:01:49 +0900

    mod/reader force quiz attempt to be submitted when timer reaches zero

--- version.php
+++ version.php
@@ -36,8 +36,8 @@ $plugin->cron      = 3600;
 $plugin->component = 'mod_reader';
 $plugin->maturity  = MATURITY_BETA; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;    // Moodle 2.0
-$plugin->release   = '2015-01-31 (61)';
-$plugin->version   = 2015013161;
+$plugin->release   = '2015-02-01 (62)';
+$plugin->version   = 2015020162;
 
 if (defined('ANY_VERSION')) {
     $plugin->dependencies = array('qtype_ordering' => ANY_VERSION);
