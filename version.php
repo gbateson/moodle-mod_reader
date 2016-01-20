51330250634b58274db4aa547cdee3ef56b95851
Gordon Bateson <gordonbateson@gmail.com>
2016-01-20 21:47:44 +0900

    mod/reader allow emails to be sent from Student (summary) report

--- version.php
+++ version.php
@@ -49,8 +49,8 @@ $plugin->cron      = 3600;
 $plugin->component = 'mod_reader';
 $plugin->maturity  = MATURITY_BETA; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;    // Moodle 2.0
-$plugin->version   = 2016012025;
-$plugin->release   = '2016-01-20 (25)';
+$plugin->version   = 2016012026;
+$plugin->release   = '2016-01-20 (26)';
 
 if (defined('ANY_VERSION')) {
     $plugin->dependencies = array('qtype_ordering' => ANY_VERSION);
