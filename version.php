18693dd059026662256d7f1d70f42735695fc443
Gordon Bateson <gordonbateson@gmail.com>
2016-07-16 12:36:51 +0900

    mod/reader Moodle coding standards compatability: remove all usage of 'print' and 'create_function' in PHP code

--- version.php
+++ version.php
@@ -40,8 +40,8 @@ $plugin->cron      = 3600;
 $plugin->component = 'mod_reader';
 $plugin->maturity  = MATURITY_BETA; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;    // Moodle 2.0
-$plugin->version   = 2016063051;
-$plugin->release   = '2016-06-30 (51)';
+$plugin->version   = 2016071652;
+$plugin->release   = '2016-07-16 (52)';
 
 if (defined('ANY_VERSION')) {
     $plugin->dependencies = array('qtype_ordering' => ANY_VERSION);
