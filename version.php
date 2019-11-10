6fbeeb82aad9ddc7d42dd1517ecae5036e657c15
Gordon Bateson <gordonbateson@gmail.com>
2019-11-10 21:06:30 +0900

    mod_reader standardize layout and spelling in CHANGES.txt

--- version.php
+++ version.php
@@ -44,8 +44,8 @@ $plugin->cron      = 3600;
 $plugin->component = 'mod_reader';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;    // Moodle 2.0
-$plugin->version   = 2019110822;
-$plugin->release   = '2019-11-08 (22)';
+$plugin->version   = 2019111023;
+$plugin->release   = '2019-11-10 (23)';
 
 if (defined('ANY_VERSION')) {
     $plugin->dependencies = array('qtype_ordering' => ANY_VERSION);
