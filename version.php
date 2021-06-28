7be0f38119f80f9c14aa641c76a74f9435ba0ddc
Gordon Bateson <gordonbateson@gmail.com>
2021-06-28 13:01:10 +0900

    mod_reader: improve compatability wth PostgreSQL (1) use AVG function instead of (SUM / COUNT); (2) use single quotes when concatenating strings

--- version.php
+++ version.php
@@ -44,8 +44,8 @@ $plugin->cron      = 3600;
 $plugin->component = 'mod_reader';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;    // Moodle 2.0
-$plugin->version   = 2021062832;
-$plugin->release   = '2021-06-28 (32)';
+$plugin->version   = 2021062834;
+$plugin->release   = '2021-06-28 (34)';
 
 if (defined('ANY_VERSION')) {
     $plugin->dependencies = array('qtype_ordering' => ANY_VERSION);
