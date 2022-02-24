1e2b1573cd1af4e434d143ad83e193fe4730b55b
Gordon Bateson <gordonbateson@gmail.com>
2022-02-24 13:17:54 +0900

    mod_reader replace used of 'groups' in SQL because it is a reserved word in MySQL 8

--- version.php
+++ version.php
@@ -44,8 +44,8 @@ $plugin->cron      = 3600;
 $plugin->component = 'mod_reader';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;    // Moodle 2.0
-$plugin->version   = 2021091435;
-$plugin->release   = '2021-09-14 (35)';
+$plugin->version   = 2022022436;
+$plugin->release   = '2022-02-24 (36)';
 
 if (defined('ANY_VERSION')) {
     $plugin->dependencies = array('qtype_ordering' => ANY_VERSION);
